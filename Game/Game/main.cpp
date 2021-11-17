ffda39afa58d55dcfe22e1b2e862f40d4497780e
Dorian chap <dorian.chapoulie@gmail.com>
2021-11-17 21:01:24 +0100

    fix git

--- Game/Game/main.cpp
+++ Game/Game/main.cpp
@@ -291,7 +291,7 @@ int main() {
 	while (!glfwWindowShouldClose(window))
 	{	
 		processInput(window);
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f); 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		dynamicsWorld->stepSimulation(timeStep, 10);
